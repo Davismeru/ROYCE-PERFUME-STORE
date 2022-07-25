c0e8e7881cc6bbce6e8b4cd5318e04711d95c70f
Davismeru <davismeru911@gmail.com>
2022-07-25 11:24:27 +0300

    added the link opening different separate content functionality

--- js/main.js
+++ js/main.js
@@ -52,3 +52,131 @@ productCategories.map((product) => {
             </div>
     `
 })
+
+function loadNewPage(whichProduct) {
+   return `<header>
+    <!-- top contacts section -->
+    <div class="contacts">
+        <div class="phone-email">
+            <i class="fa-solid fa-phone"></i></i> : <span>+254746671803</span></p>
+            <i class="fa-solid fa-envelope"></i></i><span> : davismeru911@gmail.com</span>
+        </div>
+
+        <div class="social-icons">
+            <i class="fa-brands fa-facebook-f"></i>
+            <i class="fa-brands fa-twitter"></i>
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-github"></i>
+        </div>
+    </div>
+
+    <!-- section break image -->
+    <div class="section-break-pic">
+        <img src="img/general/section png 1.png" alt="">
+    </div>
+
+    <!-- header-title -->
+    <div class="header-title">
+        <h1>ROYCE SCENTS</h1>
+    </div>
+
+    <!-- page banner -->
+    <div class="page-banner">
+        <div class="banner-header">
+            <h2>${whichProduct}</h2>
+        </div>
+    </div>
+
+    <!-- section break image -->
+    <div class="section-break-pic">
+        <img src="img/general/section png 1.png" alt="">
+    </div>
+
+</header>
+
+<!-- product best sellers categories -->
+<div class="slider best-sellers">
+    <h2>BEST <span>${whichProduct}</span></h2>
+    <div class="slider-products">
+        <!-- individual item in the best seller section -->
+        <div class="slider-item">
+            <img src="img/${whichProduct}/item-1.jpg" alt="">
+        </div>
+
+        <!-- individual item in the best seller section -->
+        <div class="slider-item">
+            <img src="img/${whichProduct}/item-2.jpg" alt="">
+        </div>
+
+        <!-- individual item in the best seller section -->
+        <div class="slider-item">
+            <img src="img/${whichProduct}/item-3.jpg" alt="">
+        </div>
+
+        <!-- individual item in the best seller section -->
+        <div class="slider-item">
+            <img src="img/${whichProduct}/item-4.jpg" alt="">
+        </div>
+    </div>
+</div>
+
+<!-- section break image -->
+<div class="section-break-pic">
+    <img src="img/general/section png 1.png" alt="">
+</div>
+
+<div class="slider slider-${whichProduct}">
+                <h2>${whichProduct}</h2>
+                <div class="slider-products">
+                    <!-- individual item in the best seller section -->
+                    <div class="slider-item">
+                        <img src="img/${whichProduct}/item-1.jpg" alt="">
+                    </div>
+
+                    <!-- individual item in the best seller section -->
+                    <div class="slider-item">
+                        <img src="img/${whichProduct}/item-2.jpg" alt="">
+                    </div>
+
+                    <!-- individual item in the best seller section -->
+                    <div class="slider-item">
+                        <img src="img/${whichProduct}/item-3.jpg" alt="">
+                    </div>
+
+                    <!-- individual item in the best seller section -->
+                    <div class="slider-item">
+                        <img src="img/${whichProduct}/item-4.jpg" alt="">
+                    </div>
+
+                    <!-- individual item in the best seller section -->
+                    <div class="slider-item">
+                        <img src="img/${whichProduct}/item-5.jpg" alt="">
+                    </div>
+
+                    <!-- individual item in the best seller section -->
+                    <div class="slider-item">
+                        <img src="img/${whichProduct}/item-6.jpg" alt="">
+                    </div>
+                </div>
+            </div>`
+}
+
+const  wrapper = document.querySelector('.wrapper')
+const productsContainer = document.querySelector('.products-container')
+const nav = document.querySelector('nav')
+const navLinks = nav.querySelectorAll('a')
+navLinks.forEach((link)=> {
+    link.addEventListener('click', (e)=> {
+        e.preventDefault()
+        const selectedProduct = e.currentTarget.innerText
+        wrapper.classList.add('not-active')
+        productsContainer.classList.remove('not-active')
+        productsContainer.innerHTML = loadNewPage(selectedProduct)
+
+
+        const bannerHeader = document.querySelector('.banner-header')
+        bannerHeader.style.background = `url(img/bracelets/banner-img.jpg) center`
+        bannerHeader.style.backgroundSize = "cover"
+    })
+})
+
